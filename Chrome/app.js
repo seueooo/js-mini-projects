d8dbb9c060032b202ed5fd1d662e5b45b27324cc
hanseo <yds06206@gmail.com>
2023-02-24 01:35:01 +0900

    Login (유저 정보 기억)

---
+++ Chrome/app.js
@@ -0,0 +1,33 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = loginForm.querySelector("input")
+
+const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting")
+const HIDDEN_CLASSNAME = "hidden"
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY,username);
+    paintGreetings();
+    
+}
+
+function paintGreetings(username) {
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerHTML = `Hello ${savedUsername}`;
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY)
+
+if(savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+
+} else {
+    paintGreetings();
+
+
+}
