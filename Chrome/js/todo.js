edd3d80452e91ed493fe43c0a737a5b5c733bb98
hanseo <yds06206@gmail.com>
2023-02-27 17:11:28 +0900

    todo-delete

--- Chrome/js/todo.js
+++ Chrome/js/todo.js
@@ -2,11 +2,22 @@ const toDoForm = document.getElementById("todo-form");
 const toDoList = document.getElementById("todo-list");
 const toDoInput = document.querySelector("#todo-form input");
 
+
+function deleteToDo(){
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintToDo(newTodo){
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span);
     span.innerText= newTodo;
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click",deleteToDo)
+    li.appendChild(span);
+    li.appendChild(button);
+
     toDoList.appendChild(li);
 }
 
@@ -17,5 +28,7 @@ function handleToDosubmit(event) {
     toDoInput.value="";
     paintToDo(newTodo);
 }
+
+
 
 toDoForm.addEventListener("submit", handleToDosubmit);
