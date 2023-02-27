d8d22a5ab3744b23200ddb0d9e3a6aba2cfe35d9
hanseo <yds06206@gmail.com>
2023-02-27 20:56:16 +0900

    todo-save

--- Chrome/js/todo.js
+++ Chrome/js/todo.js
@@ -1,6 +1,11 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoList = document.getElementById("todo-list");
 const toDoInput = document.querySelector("#todo-form input");
+let toDos = [];
+
+function saveToDos(){
+    localStorage.setItem("todos",JSON.stringify(toDos));
+}
 
 
 function deleteToDo(){
@@ -26,9 +31,21 @@ function handleToDosubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value="";
+    toDos.push(newTodo);
     paintToDo(newTodo);
+    saveToDos();
 }
 
 
 
+
 toDoForm.addEventListener("submit", handleToDosubmit);
+
+const savedToDos = localStorage.getItem("todos");
+
+if(saveToDos!==null){
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+
+}
